d8fbb16e2ff5a43abb0eb3739b3bddcd774fdf0a
Chetan <chetanrathore687@gmail.com>
2024-05-03 23:20:26 +0530

    local minima

--- src/lib/collection.ts
+++ src/lib/collection.ts
@@ -7,7 +7,7 @@ export const fetchCollection = async (): Promise<CardDetails[]> => {
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
-    return await response.json() as CardDetails[];
+    return await response.json() as CardDetails[]; // Didnt much like it. generally backend types are shared with FE but in scope of this task this is okay.
   } catch (error) {
     alert(`Error fetching data: ${ error}`);
     return [];
