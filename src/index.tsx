09e14b52f57391ce86267c10eaa419aac7d2f9da
Chetan <chetanrathore687@gmail.com>
2024-05-04 00:08:42 +0530

    some more additions

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { render } from 'react-dom';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { ROUTES } from './utils/constant';
-import Collection from './pages/Collection';
-import CreateCard from './pages/CreateCard';
+import LoadingIndicator from './components/loader';
 import Navbar from './components/navbar';
+const Collection = lazy(() => import('./pages/Collection'));
+const CreateCard = lazy(() => import('./pages/CreateCard'));
 import './style.css';
 
 const App = () => (
   <Router>
     <Navbar />
     <Switch>
-      <Route exact path={ROUTES.HOME} component={Collection} />
-      <Route exact path={ROUTES.COLLECTION} component={Collection} />
-      <Route exact path={ROUTES.CREATE_CARD} component={CreateCard} />
+      <Suspense fallback={<LoadingIndicator />}>
+        <Route exact path={ROUTES.HOME} component={Collection} />
+        <Route exact path={ROUTES.COLLECTION} component={Collection} />
+        <Route exact path={ROUTES.CREATE_CARD} component={CreateCard} />
+      </Suspense>
     </Switch>
   </Router>
 );
